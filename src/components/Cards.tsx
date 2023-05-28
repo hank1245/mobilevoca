cd54a2f2de6ba533300cd4f28f2223fe0b7b77be
Hank Kim <hank29206880@gmail.com>
2023-05-28 23:52:52 +0900

    scroll조절

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,7 +2,7 @@ import Card, { Word } from "./Card";
 
 const Cards = ({ data }: { data: Word[] }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-full pt-5 pb-10">
       {data.map((word, idx) => (
         <Card data={word} key={idx} />
       ))}
